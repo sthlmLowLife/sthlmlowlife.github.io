c4fc90e22d521fcc9405ac425e56321ef7b5938c
maximilianhagerf <max@vionlabs.com>
2014-05-19 02:11:13 +0200

    object swithcer

--- js/main_max.js
+++ js/main_max.js
@@ -5,7 +5,14 @@ $.when(
     $.getScript( "/js/vendor/three-js/build/three.min.js" ),
     $.getScript( "/js/vendor/three-js/examples/js/libs/stats.min.js" ),
     $.getScript( "/js/vendor/three-js/examples/js/Detector.js" ),
+    // === controls
+    $.getScript( "/js/vendor/three-js/examples/js/controls/TrackballControls.js" ),
+    // === loaders
+    $.getScript( "/js/vendor/three-js/examples/js/loaders/MTLLoader.js" ),
     $.getScript( "/js/vendor/three-js/examples/js/loaders/OBJLoader.js" ),
+    $.getScript( "/js/vendor/three-js/examples/js/loaders/OBJMTLLoader.js" ),
+    $.getScript( "/js/vendor/three-js/examples/js/loaders/PDBLoader.js" ),
+
     $.Deferred(function( deferred ){
         $( deferred.resolve );
     })
@@ -25,69 +32,53 @@ $.when(
     var windowHalfX = window.innerWidth / 2;
     var windowHalfY = window.innerHeight / 2;
 
+    var loader = new THREE.OBJMTLLoader();
+
+    var list_of_models = [
+
+    ["Female", "female02/female02.obj","female02/female02.mtl", 150],
+    ["Male", "male02/male02.obj","male02/male02_dds.mtl", 150],
+    ["Raptor", "raptor/Raptor.obj","raptor/Raptor.mtl", 150],
+    ["Altair", "Altair/altair.obj","Altair/star_wars.mtl", 150],
+    ["Armchair", "Armchair/Armchair.obj","Armchair/Armchair.mtl", 300]
+
+    ];
+
+    var last_item;
 
     init();
     animate();
-
+    makeMenu();
 
     function init() {
 
         container = document.createElement( 'div' );
         document.body.appendChild( container );
 
-        camera = new THREE.PerspectiveCamera( 2, window.innerWidth / window.innerHeight, 1, 2000 );
-        camera.position.z = 200;
+        camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 1, 2000 );
+        camera.position.z = 100;
 
         // scene
 
         scene = new THREE.Scene();
 
-        var ambient = new THREE.AmbientLight( 0x101030 );
+        var ambient = new THREE.AmbientLight( 0x444444 );
         scene.add( ambient );
 
         var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-        directionalLight.position.set( 0, 0, 1 );
+        directionalLight.position.set( 0, 0, 1 ).normalize();
         scene.add( directionalLight );
-
-        // texture
-
-        var manager = new THREE.LoadingManager();
-        manager.onProgress = function ( item, loaded, total ) {
-
-            console.log( item, loaded, total );
-
-        };
-
-        var texture = new THREE.Texture();
-
-        var loader = new THREE.ImageLoader( manager );
-        loader.load( '/js/vendor/three-js/examples/textures/UV_Grid_Sm.jpg', function ( image ) {
-
-            texture.image = image;
-            texture.needsUpdate = true;
-
-        } );
 
         // model
 
-        var loader = new THREE.OBJLoader( manager );
-      //  loader.load( '/js/vendor/three-js/examples/obj/male02/male02.obj', function ( object ) {
-        loader.load( '/obj/star_wars.obj', function ( object ) {
+        // loader.load( '/models/obj/' + list_of_models[0][1], '/models/obj/' + list_of_models[0][2], function ( object ) {
 
-            object.traverse( function ( child ) {
+        //     object.position.y = - 100;
+        //     scene.add( object );
 
-                if ( child instanceof THREE.Mesh ) {
+        // } );
 
-                    child.material.map = texture;
-
-                }
-
-            } );
-
-            object.position.y = 0;
-            scene.add( object );
-
-        } );
+        loadObj('/models/obj/' + list_of_models[0][1], '/models/obj/' + list_of_models[0][2], list_of_models[0][3]);
 
         //
 
@@ -101,6 +92,19 @@ $.when(
 
         window.addEventListener( 'resize', onWindowResize, false );
 
+    }
+
+    function loadObj ( obj, skin, size ) {
+        //console.log(scene.children[ 2 ]);
+        scene.remove(scene.children[ 2 ]);
+
+        loader.load( obj, skin, function ( object ) {
+            object.position.y = - 100;
+            camera.position.z = size;
+
+            last_item = scene.add( object );
+
+        } );
     }
 
     function onWindowResize() {
@@ -117,8 +121,8 @@ $.when(
 
     function onDocumentMouseMove( event ) {
 
-        mouseX = ( event.clientX - windowHalfX ) / 4;
-        mouseY = ( event.clientY - windowHalfY ) / 4;
+        mouseX = ( event.clientX - windowHalfX ) / 2;
+        mouseY = ( event.clientY - windowHalfY ) / 2;
 
     }
 
@@ -142,6 +146,24 @@ $.when(
 
     }
 
+    function makeMenu() {
+        var $someelement = $('<div id="menu"/>').appendTo('body');
+       
+        $.each(list_of_models, function( index, values) {
+            link = $('<button data-obj="'+values[1]+'" data-skin="'+values[2]+'" data-size="'+values[3]+'" >'+values[0]+'</button>').appendTo('#menu');
+            // link.click(loadNewModal(link));
+        });
+        $('#menu button').first().addClass('selected');
+        $('#menu button').click(function() {
+            $('#menu button').removeClass('selected');
+            $(this).addClass('selected');
+            obj = $(this).attr( "data-obj" );
+            skin = $(this).attr( "data-skin" );
+            size = $(this).attr( "data-size" );
+            loadObj('/models/obj/' + obj, '/models/obj/' + skin, size);
+        });
+        
+    }
 
     // ================================
     // ======== SCRIPTS LOADED - START
