229fe3f64a8a533d9ed934c7385b6819430b3979
maximilianhagerf <max@vionlabs.com>
2014-05-22 01:38:47 +0200

    städat lite

--- js/max/3d_world.js
+++ js/max/3d_world.js
@@ -2,12 +2,17 @@ $.when(
     // ================================
     // ======== SCRIPTS 
     // ================================
+
+    // === init
     $.getScript( "http://experiment1/js/vendor/three-js/build/three.min.js" ),
     $.getScript( "http://experiment1/js/vendor/three-js/examples/js/libs/stats.min.js" ),
     $.getScript( "http://experiment1/js/vendor/three-js/examples/js/Detector.js" ),
+
     // === controls
     $.getScript( "http://experiment1/js/vendor/three-js/examples/js/controls/TrackballControls.js" ),
+
     // === loaders
+    $.getScript( "http://experiment1/js/vendor/three-js/examples/js/controls/PointerLockControls.js" ),
     $.getScript( "http://experiment1/js/vendor/three-js/examples/js/loaders/MTLLoader.js" ),
     $.getScript( "http://experiment1/js/vendor/three-js/examples/js/loaders/OBJLoader.js" ),
     $.getScript( "http://experiment1/js/vendor/three-js/examples/js/loaders/OBJMTLLoader.js" ),
@@ -19,110 +24,250 @@ $.when(
 
 ).done(function(){
 
-	  // ================================
+	// ================================
     // ======== SCRIPTS LOADED - START
     // ================================
-	var container, stats;
 
-			var camera, scene, renderer;
+	var camera, scene, renderer;
+	var geometry, material, mesh;
+	var controls;
 
-			var mouseX = 0, mouseY = 0;
+	var objects = [];
 
-			var windowHalfX = window.innerWidth / 2;
-			var windowHalfY = window.innerHeight / 2;
+	var ray;
 
+	var blocker = document.getElementById( 'blocker' );
+	var instructions = document.getElementById( 'instructions' );
 
-			init();
-			animate();
+	// http://www.html5rocks.com/en/tutorials/pointerlock/intro/
 
+	var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
 
-			function init() {
+	if ( havePointerLock ) {
 
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
+		var element = document.body;
 
-				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-				camera.position.z = 200;
+		var pointerlockchange = function ( event ) {
 
-				// scene
+			if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
 
-				scene = new THREE.Scene();
+				controls.enabled = true;
 
-				var ambient = new THREE.AmbientLight( 0x444444 );
-				scene.add( ambient );
+				blocker.style.display = 'none';
 
-				var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-				directionalLight.position.set( 0, 0, 1 ).normalize();
-				scene.add( directionalLight );
+			} else {
 
-				// model
+				controls.enabled = false;
 
-				var loader = new THREE.OBJMTLLoader();
-				loader.load( 'http://experiment1/models/obj/weed/weed.obj', 'http://experiment1/models/obj/weed/weed.mtl', function ( object ) {
+				blocker.style.display = 'table';
+				blocker.style.display = '-moz-box';
+				blocker.style.display = 'box';
 
-					object.position.y = 0;
-					object.position.x = -40;
-					object.rotation.y = 1000;
-					scene.add( object );
-
-				} );
-
-				//
-
-				renderer = new THREE.WebGLRenderer();
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.setClearColor(0xFFCC99, 1);
-				container.appendChild( renderer.domElement );
-
-				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize, false );
+				instructions.style.display = '';
 
 			}
 
-			function onWindowResize() {
+		}
 
-				windowHalfX = window.innerWidth / 2;
-				windowHalfY = window.innerHeight / 2;
+		var pointerlockerror = function ( event ) {
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+			instructions.style.display = '';
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+		}
+
+		// Hook pointer lock state change events
+		document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+		document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+		document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+
+		document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+		document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+		document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+
+		instructions.addEventListener( 'click', function ( event ) {
+
+			instructions.style.display = 'none';
+
+			// Ask the browser to lock the pointer
+			element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+			if ( /Firefox/i.test( navigator.userAgent ) ) {
+
+				var fullscreenchange = function ( event ) {
+
+					if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+
+						document.removeEventListener( 'fullscreenchange', fullscreenchange );
+						document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+
+						element.requestPointerLock();
+					}
+
+				}
+
+				document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+				document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+				element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+				element.requestFullscreen();
+
+			} else {
+
+				element.requestPointerLock();
 
 			}
 
-			function onDocumentMouseMove( event ) {
+		}, false );
 
-				mouseX = ( event.clientX - windowHalfX ) / 5;
-				mouseY = ( event.clientY - windowHalfY ) / 5;
+	} else {
+
+		instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+
+	}
+
+	init();
+	animate();
+
+	function init() {
+
+		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+
+		scene = new THREE.Scene();
+		scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+
+		var light = new THREE.DirectionalLight( 0xffffff, 1.5 );
+		light.position.set( 1, 1, 1 );
+		scene.add( light );
+
+		var light = new THREE.DirectionalLight( 0xffffff, 0.75 );
+		light.position.set( -1, - 0.5, -1 );
+		scene.add( light );
+
+		controls = new THREE.PointerLockControls( camera );
+		scene.add( controls.getObject() );
+
+		ray = new THREE.Raycaster();
+		ray.ray.direction.set( 0, -1, 0 );
+
+		// floor
+
+		geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+		geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+
+		for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
+
+			var vertex = geometry.vertices[ i ];
+
+		}
+
+//floor material
+		for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+
+			var face = geometry.faces[ i ];
+		}
+
+//floor material
+		material = new THREE.MeshBasicMaterial( { color: 0x00ffff, wireframe: true  } );
+
+		mesh = new THREE.Mesh( geometry, material );
+		scene.add( mesh );
+
+		// objects
+
+
+var loader = new THREE.OBJMTLLoader();
+		loader.load( 'http://experiment1/models/obj/weed/weed.obj', 'http://experiment1/models/obj/weed/weed.mtl', function ( object ) {
+
+			object.position.y = 0;
+			object.position.x = -40;
+			object.rotation.y = 1000;
+			scene.add( object );
+
+		} );
+
+
+		geometry = new THREE.BoxGeometry( 20, 20, 20 );
+
+		for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+
+			var face = geometry.faces[ i ];
+			face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+			face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+			face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+
+		}
+
+		for ( var i = 0; i < 500; i ++ ) {
+
+			material = new THREE.MeshPhongMaterial( { specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors } );
+
+			var mesh = new THREE.Mesh( geometry, material );
+			mesh.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+			mesh.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
+			mesh.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
+			scene.add( mesh );
+
+			material.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+
+			objects.push( mesh );
+
+		}
+
+		//
+
+		renderer = new THREE.WebGLRenderer();
+		renderer.setClearColor( 0xffffff );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+
+		document.body.appendChild( renderer.domElement );
+
+		//
+
+		window.addEventListener( 'resize', onWindowResize, false );
+
+	}
+
+	function onWindowResize() {
+
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		renderer.setSize( window.innerWidth, window.innerHeight );
+
+	}
+
+	function animate() {
+
+		requestAnimationFrame( animate );
+
+		controls.isOnObject( false );
+
+		ray.ray.origin.copy( controls.getObject().position );
+		ray.ray.origin.y -= 10;
+
+		var intersections = ray.intersectObjects( objects );
+
+		if ( intersections.length > 0 ) {
+
+			var distance = intersections[ 0 ].distance;
+
+			if ( distance > 0 && distance < 10 ) {
+
+				controls.isOnObject( true );
 
 			}
 
-			//
+		}
 
-			function animate() {
+		controls.update();
 
-				requestAnimationFrame( animate );
-				render();
+		renderer.render( scene, camera );
 
-			}
+	}
 
-			function render() {
-
-				camera.position.x += ( mouseX - camera.position.x ) * .05;
-				camera.position.y += ( - mouseY - camera.position.y ) * .05;
-
-				camera.lookAt( scene.position );
-
-				renderer.render( scene, camera );
-
-			}
-   
     // ================================
-    // ======== SCRIPTS LOADED - START
+    // ======== SCRIPTS LOADED - END
     // ================================
 
 });
